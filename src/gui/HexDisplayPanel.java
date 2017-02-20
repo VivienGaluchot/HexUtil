dbe87dfaee894c90b70c439fd95086cae66e3faa
VivienGaluchot <vivien.galuchot@gmail.com>
2017-02-20 21:41:07 +0100

    HexDisplayPanel

--- src/gui/HexDisplayPanel.java
+++ src/gui/HexDisplayPanel.java
@@ -1,12 +1,73 @@
 package gui;
 
-import javax.swing.JPanel;
+import java.awt.Component;
+import java.awt.Font;
+import java.awt.GridBagLayout;
 
-public class HexDisplayPanel extends JPanel {
+import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.JLabel;
+import javax.swing.JList;
+import javax.swing.ListCellRenderer;
+
+import data.HexLine;
+import data.LineProcessor;
+import data.Tools;
+
+public class HexDisplayPanel extends JList<HexLine>implements LineProcessor {
 	private static final long serialVersionUID = 1L;
+
+	DefaultListModel<HexLine> hexLineJListModel;
 
 	public HexDisplayPanel() {
 		super();
+		setLayout(new GridBagLayout());
 
+		hexLineJListModel = new DefaultListModel<>();
+		setModel(hexLineJListModel);
+		setCellRenderer(new HexLineCellRenderer());
 	}
+
+	public void addLine(HexLine l) {
+		hexLineJListModel.addElement(l);
+		repaint();
+	}
+
+	public void clearLines() {
+		hexLineJListModel.removeAllElements();
+	}
+
+	@Override
+	public void processLine(HexLine l) {
+		addLine(l);
+	}
+
+	private class HexLineCellRenderer implements ListCellRenderer<HexLine> {
+
+		private DefaultListCellRenderer defaultRenderer = new DefaultListCellRenderer();
+
+		@Override
+		public Component getListCellRendererComponent(JList<? extends HexLine> list, HexLine value, int index,
+				boolean isSelected, boolean cellHasFocus) {
+			StringBuffer buf = new StringBuffer();
+
+			int maxNb = (int) Math.log10(list.getModel().getSize()) + 1;
+			buf.append(String.format("<html><FONT COLOR=\"#888888\">%0" + maxNb + "d</FONT> ", index));
+			buf.append("<FONT STYLE=\"background:#FFFFCC\">:</FONT>");
+			buf.append(String.format("<FONT STYLE=\"background:#CCFFCC\">%02X</FONT>", value.getSize()));
+			buf.append(String.format("<FONT STYLE=\"background:#CCCCFF\">%04X</FONT>", value.getAddress()));
+			buf.append(String.format("<FONT STYLE=\"background:#FFCCCC\">%02X</FONT>", value.getType()));
+			buf.append("<FONT STYLE=\"background:#CCFFFF\">" + Tools.hexToString(value.getData()) + "</FONT>");
+			if(value.isValid())
+				buf.append(String.format("<FONT STYLE=\"background:#CCCCCC\">%02X</FONT></html>", value.getChecksum()));
+			else
+				buf.append(String.format("<FONT STYLE=\"background:#550000; color:#FF0000\"><S>%02X</S></FONT></html>", value.getChecksum()));
+			JLabel label = (JLabel) defaultRenderer.getListCellRendererComponent(list, value, index, isSelected,
+					cellHasFocus);
+			label.setText(buf.toString());
+			label.setFont(new Font("Consolas", Font.PLAIN, 13));
+			return label;
+		}
+	}
+
 }
--- src/gui/HexUtilGui.java
+++ src/gui/HexUtilGui.java
@@ -1,16 +1,77 @@
 package gui;
 
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
+import javax.swing.filechooser.FileFilter;
 
-public class HexUtilGui {
+import data.HexFile;
+
+public class HexUtilGui implements ActionListener {
+
+	private static String ACTION_OPEN = "action_open";
+	private static String ACTION_QUIT = "action_quit";
 
 	private JFrame mainFrame;
+	private HexDisplayPanel hexDisplayPanel;
+
+	public static void main(String[] args) {
+		HexUtilGui x = new HexUtilGui();
+		x.loadFile(new File("test.hex"));
+	}
 
 	public HexUtilGui() {
 		mainFrame = new JFrame();
 		mainFrame.setTitle("HexUtil");
+
+		mainFrame.setLayout(new GridBagLayout());
+
+		// MenuBar
+		JMenuBar menuBar = new JMenuBar();
+		JMenu menu = new JMenu("Fichier");
+
+		JMenuItem item = new JMenuItem("Ouvrir");
+		item.setActionCommand(ACTION_OPEN);
+		item.addActionListener(this);
+		menu.add(item);
+
+		item = new JMenuItem("Quitter");
+		item.setActionCommand(ACTION_QUIT);
+		item.addActionListener(this);
+		menu.add(item);
+
+		menuBar.add(menu);
+		mainFrame.setJMenuBar(menuBar);
+
+		// MapPanem
+		JPanel panel = new JPanel();
+		panel.setMinimumSize(new Dimension(150, 150));
+		panel.setPreferredSize(new Dimension(300, 300));
+
+		// DisplayPanel
+		hexDisplayPanel = new HexDisplayPanel();
+		JScrollPane scroll = new JScrollPane(hexDisplayPanel);
+		scroll.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		scroll.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		JSplitPane split = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, panel, scroll);
+		mainFrame.add(split, new GridBagConstraints(0, 0, 1, 1, 1, 1, GridBagConstraints.CENTER,
+				GridBagConstraints.BOTH, new Insets(0, 0, 0, 0), 0, 0));
+		split.setDividerLocation(800 - 300);
+		split.setResizeWeight(1);
 
 		mainFrame.setSize(new Dimension(800, 600));
 		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -19,7 +80,51 @@ public class HexUtilGui {
 		mainFrame.setLocationRelativeTo(null);
 	}
 
-	public static void main(String[] args) {
-		new HexUtilGui();
+	public void loadFile(File file) {
+		HexFile hf = new HexFile();
+		hf.addProcessor(hexDisplayPanel);
+		hexDisplayPanel.clearLines();
+		hf.processFile(file.getAbsolutePath());
+	}
+
+	// Actions
+
+	private static File lastOpennedPath = null;
+
+	public void action_open() {
+		JFileChooser fc = new JFileChooser();
+		if (lastOpennedPath != null)
+			fc.setSelectedFile(lastOpennedPath);
+		fc.addChoosableFileFilter(new FileFilter() {
+			@Override
+			public boolean accept(File file) {
+				return file.getName().toLowerCase().endsWith(".hex");
+			}
+
+			@Override
+			public String getDescription() {
+				return "Fichier .HEX (Intel)";
+			}
+		});
+		int r = fc.showOpenDialog(mainFrame);
+		if (r == JFileChooser.APPROVE_OPTION) {
+			lastOpennedPath = fc.getSelectedFile();
+			loadFile(lastOpennedPath);
+		}
+	}
+
+	public void action_quit() {
+		System.exit(0);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		String ae = arg0.getActionCommand();
+		if (ae.compareTo(ACTION_OPEN) == 0)
+			action_open();
+		else if (ae.compareTo(ACTION_QUIT) == 0)
+			action_quit();
+		else
+			System.out.println("Unknown action : " + ae);
 	}
 }
